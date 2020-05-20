e1f3f968420c1bede2e1caa9110c30824495b58d
Magatto <maksimov.promo@gmail.com>
2020-05-20 22:13:11 +0300

    Update webhook.go

--- webhook.go
+++ webhook.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("1151152699:AAGpIAx7ek89UoDca-**************")
+	bot, err := tgbotapi.NewBotAPI("1151152699:AAGpIAx7&&&***xZmhVvouk")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,6 +26,32 @@ func main() {
 	go http.ListenAndServeTLS("213.139.208.21:8443", "cert.pem", "key.pem", nil)
 
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
+
+		// Create a new MessageConfig. We don't have text yet,
+		// so we should leave it empty.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "type /sayhi or /status."
+		case "sayhi":
+			msg.Text = "Hi :)"
+		case "status":
+			msg.Text = "I'm ok."
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
 	}
 }
