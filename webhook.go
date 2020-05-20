a48a85753469e24870137b509987398dc238ecd3
Magatto <maksimov.promo@gmail.com>
2020-05-20 23:35:58 +0300

    Update webhook.go

--- webhook.go
+++ webhook.go
@@ -1,57 +1,36 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("1151152699:AAGpIAx7&&&***xZmhVvouk")
-	if err != nil {
-		log.Fatal(err)
-	}
+    bot, err := tgbotapi.NewBotAPI("Мой токен")
+    if err != nil {
+        log.Panic(err)
+    }
 
-	bot.Debug = true
+    bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+    log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("http://213.139.208.21:8443/"+bot.Token, "cert.pem"))
-	if err != nil {
-		log.Fatal(err)
-	}
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
 
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS("213.139.208.21:8443", "cert.pem", "key.pem", nil)
+    updates, err := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message updates
-			continue
-		}
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message Updates
+            continue
+        }
 
-		if !update.Message.IsCommand() { // ignore any non-command Messages
-			continue
-		}
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+        msg.ReplyToMessageID = update.Message.MessageID
 
-		// Create a new MessageConfig. We don't have text yet,
-		// so we should leave it empty.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
-		// Extract the command from the Message.
-		switch update.Message.Command() {
-		case "help":
-			msg.Text = "type /sayhi or /status."
-		case "sayhi":
-			msg.Text = "Hi :)"
-		case "status":
-			msg.Text = "I'm ok."
-		default:
-			msg.Text = "I don't know that command"
-		}
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
-		}
-	}
+        if _, err := bot.Send(msg); err != nil {
+            log.Panic(err)
+        }
+    }
 }
